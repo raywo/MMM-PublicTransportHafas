db0be95e6b4284aa753dee348d2ff8f4a11b6242
AgP42 <12719820+AgP42@users.noreply.github.com>
2018-07-27 19:50:20 +0200

    Minor - initialization of variable on start

--- MMM-PublicTransportHafas.js
+++ MMM-PublicTransportHafas.js
@@ -60,9 +60,9 @@ Module.register("MMM-PublicTransportHafas", {
   start: function () {
     Log.info("Starting module: " + this.name + " with identifier: " + this.identifier);
     
-    var ModulePublicTransportHafasHidden = false; // par défaut on affiche le module (si pas de module carousel ou autre)
-	this.updatesIntervalID = 0; 	// to stop and start auto update for each module instance
-	var lastUpdate = 0; 	//timestamp of the last module update. set at 0 at start-up
+    this.ModulePublicTransportHafasHidden = false; // par défaut on affiche le module (si pas de module carousel ou autre)
+    this.updatesIntervalID = 0; 	// to stop and start auto update for each module instance
+    this.lastUpdate = 0; 	//timestamp of the last module update. set at 0 at start-up
 
     this.departures = [];
     this.initialized = false;
